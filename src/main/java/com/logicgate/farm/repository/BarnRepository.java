3458559da0016035ff33000bd3f96f012567ca95
Josiah <jones.josiah93@gmail.com>
2019-09-13 10:47:52 -0500

    Fix formatting

--- src/main/java/com/logicgate/farm/repository/BarnRepository.java
+++ src/main/java/com/logicgate/farm/repository/BarnRepository.java
@@ -3,15 +3,15 @@ package com.logicgate.farm.repository;
 import com.logicgate.farm.domain.Barn;
 
 import com.logicgate.farm.domain.Color;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BarnRepository extends JpaRepository<Barn, Long> {
-    List<Barn> findAllByColor(Color color);
+  List<Barn> findAllByColor(Color color);
 
-    // additional methods can be defined here
+  // additional methods can be defined here
 
 }
